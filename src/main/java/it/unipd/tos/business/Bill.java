df51c2d6a04971ed3d60f4a673e3528efa7f6f77
stefano <stefano.sgt39@gmail.com>
2018-12-10 20:44:09 +0100

    closes #9

--- src/main/java/it/unipd/tos/business/Bill.java
+++ src/main/java/it/unipd/tos/business/Bill.java
@@ -1,5 +1,6 @@
 package it.unipd.tos.business;
 
+import java.util.Comparator;
 import java.util.List;
 
 import it.unipd.tos.business.exception.RestaurantBillException;
@@ -9,11 +10,25 @@ public class Bill implements RestaurantBill {
 
 	@Override
 	public double getOrderPrice(List<MenuItem> itemsOrdered) throws RestaurantBillException {
-		
-		double price = itemsOrdered.stream()
-			.map((item)->item.price())
-			.reduce(0.0,Double::sum);
-		return price;
-	}
 
+		/*
+		 * calcolo somma semplice issue #8
+		 */
+		double price = itemsOrdered.stream().map((item) -> item.price()).reduce(0.0, Double::sum);
+
+		/*
+		 * sconto 10 pizze issue #9
+		 */
+		if (itemsOrdered.size() > 10) {
+			price = price - itemsOrdered.stream().map((item) -> item.price()).min(Comparator.comparing(Double::valueOf))
+					.get();
+		}
+
+		if (price > 100) {
+			price = price * 0.95;
+		}
+
+		return price;
+
+	}
 }
--- src/test/java/it/unipd/tos/model/MenuItemTest.java
+++ src/test/java/it/unipd/tos/model/MenuItemTest.java
@@ -12,25 +12,69 @@ import it.unipd.tos.business.exception.RestaurantBillException;
 public class MenuItemTest {
 
 	/*
-	 * issue #8 . Controlla un ordine semplice (senza offerte) 
+	 * issue #8 . ordine semplice (senza offerte)
 	 */
 	@Test
 	public void sum_smallOrderPrice() {
 		Bill bill = new Bill();
 
 		ArrayList<MenuItem> order = new ArrayList<MenuItem>();
-		
-		order.add( new MenuItem(MenuItem.itemType.Pizze,"Verdure", 6.50));
-		order.add( new MenuItem(MenuItem.itemType.Pizze,"Tonno e Cipolle", 6.00));
-		order.add( new MenuItem(MenuItem.itemType.Pizze,"Ananas", 1.50));
-		order.add( new MenuItem(MenuItem.itemType.Primi,"Risotto ai funghi", 7.50));
-		
+
+		order.add(new MenuItem(MenuItem.itemType.Pizze, "Verdure", 6.50));
+		order.add(new MenuItem(MenuItem.itemType.Pizze, "Tonno e Cipolle", 6.00));
+		order.add(new MenuItem(MenuItem.itemType.Pizze, "Ananas", 1.50));
+		order.add(new MenuItem(MenuItem.itemType.Primi, "Risotto ai funghi", 7.50));
+
 		try {
-			assertEquals(bill.getOrderPrice(order),21.50,0);
+			assertEquals(21.50, bill.getOrderPrice(order), 0);
 		} catch (RestaurantBillException e) {
 			e.printStackTrace();
 		}
-		
+	}
+
+	/*
+	 * issue #9 . ordine con >10 ordinazioni
+	 */
+	@Test
+	public void sum_moreThen10ItemsPrice() {
+		Bill bill = new Bill();
+
+		ArrayList<MenuItem> order = new ArrayList<MenuItem>();
+
+		order.add(new MenuItem(MenuItem.itemType.Pizze, "Verdure", 6.50));
+		order.add(new MenuItem(MenuItem.itemType.Pizze, "Tonno e Cipolle", 6.00));
+		order.add(new MenuItem(MenuItem.itemType.Pizze, "Ananas", 1.50));
+		order.add(new MenuItem(MenuItem.itemType.Primi, "Risotto ai funghi", 7.50));
+		order.add(new MenuItem(MenuItem.itemType.Primi, "Pasta", 4.20));
+		order.add(new MenuItem(MenuItem.itemType.Primi, "Riso", 3.80));
+		order.add(new MenuItem(MenuItem.itemType.Primi, "Minestrone", 4.00));
+		order.add(new MenuItem(MenuItem.itemType.Primi, "Minestra", 2.00));
+		order.add(new MenuItem(MenuItem.itemType.Pizze, "Viennese", 6.50));
+		order.add(new MenuItem(MenuItem.itemType.Pizze, "Margherita", 4.00));
+		order.add(new MenuItem(MenuItem.itemType.Pizze, "Calzone", 6.50));
+		try {
+			assertEquals(51.00, bill.getOrderPrice(order), 0);
+		} catch (RestaurantBillException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * issue #9 . ordine con >10 ordinazioni
+	 */
+	@Test
+	public void sum_moreThen100euroOrderSalePrice() {
+		Bill bill = new Bill();
+
+		ArrayList<MenuItem> order = new ArrayList<MenuItem>();
+
+		order.add(new MenuItem(MenuItem.itemType.Pizze, "Verdure", 100.00));
+		order.add(new MenuItem(MenuItem.itemType.Pizze, "Tonno e Cipolle", 20.0));
+		try {
+			assertEquals(114, bill.getOrderPrice(order), 0);
+		} catch (RestaurantBillException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
